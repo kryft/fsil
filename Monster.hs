5b57574230b9ffdee37761dc30e516a6ccabe1d3
Kristian Nybo <kryfti@gmail.com>
2013-08-19 10:04:23 +0300

    Added getMonster for selecting a monster from a list with a regex

--- Monster.hs
+++ Monster.hs
@@ -1,11 +1,23 @@
 module Monster where
 
 import Dice
+import Data.List (null, filter)
+import Text.Regex.Posix ((=~))
 
 data Monster = Monster { monsName :: String, 
                          monsAttacks :: [(Int,Dice)], 
                          monsEvasion :: Int,
                          monsProtDice :: Dice,
                          monsSpeed :: Int
-              } deriving Show
+                       } deriving Show
 
+
+matchMonster :: String -> Monster -> Bool
+matchMonster regex mons = (monsName mons) =~ regex
+
+getMonster :: String -> [Monster] -> Monster
+getMonster nameRegex monsList = 
+  let matchingMonsters = filter (matchMonster nameRegex) monsList
+  in if null matchingMonsters
+        then head monsList
+        else head matchingMonsters
