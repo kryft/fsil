73deb8f148cc28a3adb92b15de3f3e78dce8f1f5
Kristian Nybo <kryfti@gmail.com>
2013-08-19 10:07:42 +0300

    Added fight: gives damage distributions for the player and the monster. Currently the player's stats need to be input manually, but this will change once the chardump parser is done.

--- Fsil.hs
+++ Fsil.hs
@@ -1,10 +1,22 @@
 import Control.Monad
 import Numeric
-import Data.List
+import Data.List ((!!))
 import qualified Numeric.Probability.Distribution as D
 import qualified Numeric.Probability.Percentage as Percentage
+import qualified Monster as M
+import MonsterParser
 
 import Dice
+
+monsBaseCritThres = 7
+
+data FightStats = FightStats { damGiven :: Dist, 
+                               damTaken :: Dist, 
+                               player :: String, 
+                               opponent :: M.Monster
+                             }
+
+
 
 toHitDist :: Int -> Int -> Dist
 toHitDist accuracy evasion = 
@@ -33,10 +45,23 @@ attackDist accuracy damDice baseCriticalThreshold evasion
   do
     toHit <- D.norm $ toHitDist accuracy evasion
     if toHit < 1 then
-      return 0
+      D.certainly 0
     else do
       let damDice' = addDice nCritDice damDice
           nCritDice = nCrits baseCriticalThreshold toHit
       D.norm $ damageDist damDice' protectionDice
 
 
+fight :: Int -> Dice -> Int -> [Dice] -> Double -> String -> Int -> [M.Monster] -> FightStats
+fight acc damDice ev protDice baseCritThres monsName monsAttackNumber monsList =
+  let opponent' = M.getMonster monsName monsList
+      (oppAcc,oppDam) = (M.monsAttacks opponent') !! monsAttackNumber
+      oppEv = M.monsEvasion opponent'
+      oppProt = M.monsProtDice opponent'
+      damGiven' = attackDist acc damDice baseCritThres oppEv [oppProt]
+      oppBaseCritThres = monsBaseCritThres + 2 * fromIntegral (nDice oppDam)
+      damTaken' = attackDist oppAcc oppDam oppBaseCritThres ev protDice
+  in
+    FightStats { damGiven = damGiven', damTaken = damTaken', 
+                 player = "player", opponent = opponent' }
+     
